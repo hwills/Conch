1a6a462746784993abaf3607a09986e2b20e1623
= <=>
2015-09-28 01:23:21 -0500

    Morgan worked by himself for a bit, starting from scratch.  This should be strictly better than what we had, with the exception that we no longer have a Makefile (and I'm make-illiterate).

---
+++ InterpretCommand.h
@@ -0,0 +1,187 @@
+//
+//  InterpretCommand.h
+//  ShellFish
+//
+//  Created by Morgan Redding on 9/24/15.
+//  Copyright © 2015 Morgan Redding. All rights reserved.
+//
+
+#ifndef InterpretCommand_h
+#define InterpretCommand_h
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include "Variable.h"
+
+// if there are multiple spaces in a row, kill them all except one of them; spaces on the ends are killed entirely
+// the only exception is that if the spaces are between quotes, they should be preserved
+std::string remove_excess_spaces(const std::string& txt) {
+    std::string rtn = "";
+    char inside_quotes = 0;
+    for(int i = 0; i < txt.size(); i++) {
+        if(txt[i] == '\\') {
+            rtn += txt[i];
+            i++;
+            rtn += txt[i];
+        }
+        else if(txt[i] == '\'' && inside_quotes != '"') {
+            inside_quotes = inside_quotes == '\'' ? 0 : '\'';
+            rtn += txt[i];
+        }
+        else if(txt[i] == '"' && inside_quotes != '\'') {
+            inside_quotes = inside_quotes == '"' ? 0 : '"';
+            rtn += txt[i];
+        }
+        else if(inside_quotes == 0 && txt[i] == ' ') {
+            if(rtn[rtn.size() - 1] != ' ') {
+                rtn += ' ';
+            }
+        }
+        else {
+            rtn += txt[i];
+        }
+    }
+    if(rtn[0] == ' ') { // remove a space if it is the first character
+        rtn = rtn.substr(1, rtn.size() - 1);
+    }
+    if(rtn[rtn.size() - 1] == ' ') { // remove a space if it is the last character
+        rtn = rtn.substr(0, rtn.size() - 1);
+    }
+
+    return rtn;
+}
+
+// returns 'true' for numbers (0 - 9)
+bool is_numeric(char c) {
+    if(c < 48) {
+        return false;
+    }
+    return c < 58;
+}
+
+// returns 'true' for alphanumeric characters and underscore
+bool isLegalVariableCharacter(char c) {
+    if(c < 47) {
+        return false;
+    }
+    if(c < 58) {
+        return true;
+    }
+    if(c < 65) {
+        return false;
+    }
+    if(c < 91) {
+        return true;
+    }
+    if(c == 95) {
+        return true;
+    }
+    if(c < 97) {
+        return false;
+    }
+    if(c < 123) {
+        return true;
+    }
+    return false;
+}
+
+// splits a string by a character, ignoring characters between quotes
+std::vector<std::string> split_but_preserve_literal_strings(const std::string& txt, const char symbol_to_split_by) {
+
+    std::vector<std::string> rtn;
+    std::string t = "";
+
+    for(int i = 0; i < txt.size(); i++) {
+        if(txt[i] == '\\') {
+            t += txt[i];
+            i++;
+            t += txt[i];
+            continue;
+        }
+        if(txt[i] == '\'') {
+            i++;
+            while(i < txt.size() && txt[i] != '\'') {
+                if(txt[i] == '\\') {
+                    t += txt[i];
+                    i++;
+                }
+                t += txt[i];
+                i++;
+            }
+        }
+        else if(txt[i] == '"') {
+            i++;
+            while(i < txt.size() && txt[i] != '"') {
+                if(txt[i] == '\\') {
+                    t += txt[i];
+                    i++;
+                }
+                t += txt[i];
+                i++;
+            }
+        }
+        else if(txt[i] == symbol_to_split_by) {
+            if(t != "") {
+                rtn.push_back(t);
+                t = "";
+            }
+        }
+        else {
+            t += txt[i];
+        }
+    }
+    
+    rtn.push_back(t);
+    return rtn;
+}
+
+std::vector<std::string> split_string(const std::string& txt, const char symbol_to_split_by) {
+    std::vector<std::string> rtn;
+    int start = 0;
+    for(int i = 0; i < txt.size(); i++) {
+        if(txt[i] == symbol_to_split_by) {
+            while(i < txt.size() && txt[i] != symbol_to_split_by) {
+                i++;
+            }
+            rtn.push_back(txt.substr(start, i - start));
+            start = i + 1;
+        }
+    }
+    rtn.push_back(txt.substr(start, txt.size() - start));
+    return rtn;
+}
+
+std::string substituteVariableValues(const std::string& txt, const std::unordered_map<std::string, Variable>& localVariables, const std::unordered_map<std::string, Variable>& globalVariables) {
+    std::string rtn = "";
+    for(int i = 0; i < txt.size(); i++) {
+        if(txt[i] == '$') {
+            i++;
+            int start = i;
+            while(i < txt.size() && isLegalVariableCharacter(txt[i])) {
+                i++;
+            }
+            std::unordered_map<std::string, Variable>::const_iterator it = localVariables.find(txt.substr(start, i - start));
+            if(it == localVariables.end()) {
+                it = globalVariables.find(txt.substr(start, i - start));
+                if(it == globalVariables.end()) {
+                    i = start;
+                    rtn += '$';
+                }
+                else {
+                    rtn += it->second.value_as_string();
+                }
+            }
+            else {
+                rtn += it->second.value_as_string();
+            }
+            i--;
+        }
+        else {
+            rtn += txt[i];
+        }
+    }
+    return rtn;
+}
+
+#endif /* InterpretCommand_h */
---
+++ main.cpp
@@ -0,0 +1,103 @@
+//
+//  main.cpp
+//  ShellFish
+//
+//  Created by Morgan Redding on 9/24/15.
+//  Copyright © 2015 Morgan Redding. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include "Variable.h"
+#include "InterpretCommand.h"
+
+std::string find_file(const std::string& file_name) {
+    // TODO
+    return "";
+}
+
+std::string execute_file(const std::string& file_name) {
+    // TODO
+    return "\"[insert output from file " + file_name  + " here]\"";
+}
+
+std::string execute(const std::string& command, int debug_level, std::unordered_map<std::string, Variable>& localVariables, std::unordered_map<std::string, Variable>& globalVariables) {
+    
+    std::vector<std::string> command_parts = split_but_preserve_literal_strings(command, ' ');
+
+    if(command_parts[0] == "-f") {
+        return execute_file(command.substr(3, command.size()));
+    }
+    if(command_parts[0] == "set") {
+        localVariables[command_parts[1]] = Variable(command_parts[2]);
+        return "";
+    }
+    if(command_parts[0] == "unset") {
+        localVariables.erase(command_parts[1]);
+        return "";
+    }
+    if(command_parts[0] == "echo") {
+        return command_parts[1];
+    }
+    return "UNKNOWN COMMAND: " + command_parts[0];
+}
+
+int main(int argc, const char * argv[]) {
+    
+    std::unordered_map<std::string, Variable> localVariables;
+    std::unordered_map<std::string, Variable> globalVariables;
+    
+    localVariables["local1"] = Variable("a");
+    globalVariables["global1"] = Variable("b");
+    localVariables["var"] = Variable("a");
+    globalVariables["var"] = Variable("b");
+    
+    while(true) {
+        
+        int debug_level;
+        std::string command;
+        
+        std::cout << "sish >> ";
+        std::getline(std::cin, command);
+        
+        command = remove_excess_spaces(command);
+        
+        std::string command_after_var_substitution = substituteVariableValues(command, localVariables, globalVariables);
+        
+        // take care of '-x' flag
+        if(command_after_var_substitution.substr(0, 2) == "-x") {
+            command_after_var_substitution = command_after_var_substitution.substr(3, command_after_var_substitution.size());
+            std::cout << command_after_var_substitution << std::endl;
+        }
+        
+        // take care of '-d' flag (debug level)
+        if(command_after_var_substitution.substr(0, 2) == "-d") {
+            int i = 3;
+            while(i < command_after_var_substitution.size() && is_numeric(command_after_var_substitution[i])) {
+                i++;
+            }
+            try {
+                debug_level = std::stoi(command_after_var_substitution.substr(3, i - 3));
+            }
+            catch(const std::exception& e) {
+                std::cout << "Error in reading value for DEBUG_LEVEL" << std::endl;
+                continue;
+            }
+            command_after_var_substitution = command_after_var_substitution.substr(i + 1, command_after_var_substitution.size());
+        }
+        else {
+            debug_level = 0;
+        }
+
+        
+        std::vector<std::string> individual_commands = split_string(command_after_var_substitution, '|');
+        
+        std::string output = "";
+        for(int i = 0; i < individual_commands.size(); i++) {
+            output = execute(individual_commands[i] + " " + output, debug_level, localVariables, globalVariables);
+        }
+        std::cout << output << "\n";
+    }
+
+    return 0;
+}
